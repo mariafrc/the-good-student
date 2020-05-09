8e9b5cabb0e6cbf64fab27feaae448480bd8b601
franckBemaria <franckbemaria@gmail.com>
2020-05-09 07:21:22 +0300

    feat: note preview
    page pour consulter une note en particulier

---
+++ src/app/note/note-preview/note-preview.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import {NoteService, Note} from '../../services/note/note.service'
+import {ActivatedRoute, Router} from '@angular/router'
+import { AlertController } from '@ionic/angular';
+import {ModalController} from '@ionic/angular'
+import {NoteFormComponent} from '../modal/note-form/note-form.component'
+
+@Component({
+  selector: 'app-note-preview',
+  templateUrl: './note-preview.component.html',
+  styleUrls: ['./note-preview.component.scss'],
+})
+export class NotePreviewComponent implements OnInit {
+	note: Note
+  constructor(
+  	private noteService: NoteService,
+  	private route: ActivatedRoute,
+    private alertController: AlertController,
+    private modalCtrl: ModalController,
+    private router: Router
+  ) { }
+
+  async ngOnInit() {
+    //await this.noteService.beforeUse()
+  	this.note = await this.noteService.getNote(this.route.snapshot.params['id'])
+    if(!this.note)
+      this.router.navigate(['/note'])
+  }
+
+  async onShowForm(){
+    const modal = await this.modalCtrl.create({
+      component: NoteFormComponent,
+      componentProps: {
+        action: 'edit',
+        note: {...this.note}
+      }
+    })
+
+    await modal.present()
+
+    const {data} = await modal.onWillDismiss()
+    if(data){
+      this.note = await this.noteService.getNote(this.route.snapshot.params['id'])
+    }
+  }
+
+  async onDeleteNote(){
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: 'Voulez vous vraiment supprimer cette note?',
+      buttons: [
+        {
+          text: 'Non',
+          role: 'cancel'
+        }, {
+          text: 'Oui',
+          handler: async () => {
+            await this.noteService.deleteNote(this.note.id)
+            this.router.navigate(['/note'])
+          }
+        }
+      ]
+    })
+
+    await alert.present()
+  }
+
+}
--- src/app/note/note.module.ts
+++ src/app/note/note.module.ts
@@ -5,15 +5,18 @@ import { FormsModule } from '@angular/forms'
 import { IonicModule } from '@ionic/angular';
 import {NoteHomeComponent} from './note-home/note-home.component'
 import {NoteFormComponent} from './modal/note-form/note-form.component'
+import {NotePreviewComponent} from './note-preview/note-preview.component'
 
 const routes: Routes = [
 	{path: '', component: NoteHomeComponent},
+  {path: ':id', component: NotePreviewComponent}
 ];
 
 @NgModule({
   declarations: [
   	NoteHomeComponent,
     NoteFormComponent,
+    NotePreviewComponent
   ],
   entryComponents: [
     NoteFormComponent
