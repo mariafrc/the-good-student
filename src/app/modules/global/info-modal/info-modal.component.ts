a2c39c74464ee279532b7be502c85eb2de962595
franckBemaria <franckbemaria@gmail.com>
2020-07-18 09:55:59 +0300

    feat: project spec

--- src/app/modules/global/info-modal/info-modal.component.ts
+++ src/app/modules/global/info-modal/info-modal.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
+import {version} from '../../../../../package.json'
 @Component({
   selector: 'app-info-modal',
   templateUrl: './info-modal.component.html',
   styleUrls: ['./info-modal.component.scss'],
 })
 export class InfoModalComponent implements OnInit {
-
+	version: string = version
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
 }
