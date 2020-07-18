f8f552e77f61de10681ffb04ac618e9f4458092d
franckBemaria <franckbemaria@gmail.com>
2020-07-18 09:59:25 +0300

    style: style and ressource update
    app icon, font and layout bug

--- src/app/modules/global/info-modal/info-modal.component.ts
+++ src/app/modules/global/info-modal/info-modal.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-import {version} from '../../../../../package.json'
+
 @Component({
   selector: 'app-info-modal',
   templateUrl: './info-modal.component.html',
   styleUrls: ['./info-modal.component.scss'],
 })
 export class InfoModalComponent implements OnInit {
-	version: string = version
+
   constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
